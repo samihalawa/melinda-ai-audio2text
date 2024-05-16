e570205ea42a96fa830342dc912dd872235ac54d
Sami Halawa <samihalawa@mac.local>
2024-05-16 23:04:01 +0200

    m

---
+++ withshare/service-worker.js
@@ -0,0 +1,62 @@
+const CACHE_NAME = 'melindaai-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/script.js',
+  '/icons/android-chrome-192x192.png',
+  '/icons/android-chrome-512x512.png',
+  '/icons/apple-touch-icon.png',
+  '/icons/favicon-32x32.png',
+  '/icons/favicon-16x16.png',
+  '/icons/mstile-150x150.png',
+  '/icons/safari-pinned-tab.svg',
+  // Add any other assets you want to cache
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(keyList.map(key => {
+        if (!cacheWhitelist.includes(key)) {
+          return caches.delete(key);
+        }
+      }))
+    )
+  );
+});
+
+self.addEventListener('message', event => {
+  if (event.data && event.data.type === 'SHARE_TARGET') {
+    handleShareTarget(event.data.files);
+  }
+});
+
+async function handleShareTarget(files) {
+  for (const file of files) {
+    // Post a message to the client to start the transcription process
+    self.clients.matchAll().then(clients => {
+      clients.forEach(client => {
+        client.postMessage({
+          type: 'TRANSCRIBE_FILE',
+          file: file
+        });
+      });
+    });
+  }
+}
